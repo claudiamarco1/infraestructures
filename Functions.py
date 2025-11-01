462e93cd4fcc2ac502e289a9d2e75b815b4ac16b
Pol Pujols Casellas <58744270+polpujols@users.noreply.github.com>
2025-11-01 13:26:01 +0100

    Implement api_etl function for API data extraction
    Function to extract data from randomuser.me API and return as JSON.

---
+++ Functions.py
@@ -0,0 +1,27 @@
+# Como llamar la funcion:
+# from Functions import api_etl
+import requests
+
+def api_etl(url: str, results: int, seed: str):
+    """
+    Función para extraer los datos de dentro de randomuser.me API y devolverlos en formato JSON.
+
+    Parameters
+    ----------
+    url : str
+        API Link "https://randomuser.me/api"
+    results : int
+        Numero de usuarios a extraer (e.g., 500)
+    seed : str
+        Seed valor para generar el mismo set de usuarios.
+    """
+    params: Dict[str, str | int] = {
+        "results": results, # El resultado de Nº users que queremos extraer
+        "seed": seed,       # Seeds permite generar la misma seleccion de usuarios.
+        "format": "json"    # JSON,CSV,XML,YAML output
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()   # Si requests falla nos da información con un mensaje de error
+
+    return response.json()
